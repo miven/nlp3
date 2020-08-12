6a0de6a0e038439c844f05b510dd16b6dd59f6fc
zhangbo2008 <15122306087@163.com>
2020-08-12 09:27:11 +0800

    no_comment

--- 研究yolov4/pytorch-YOLOv4-master/train.py
+++ 研究yolov4/pytorch-YOLOv4-master/train.py
@@ -38,6 +38,7 @@ from tool.tv_reference.coco_utils import convert_to_coco_api
 from tool.tv_reference.coco_eval import CocoEvaluator
 
 #iou算法.
+# 可以看这个文章:https://zhuanlan.zhihu.com/p/94799295
 def bboxes_iou(bboxes_a, bboxes_b, xyxy=True, GIoU=False, DIoU=False, CIoU=False):
     """Calculate the Intersection of Unions (IoUs) between bounding boxes.
     IoU is calculated as a ratio of area of the intersection
@@ -60,15 +61,16 @@ def bboxes_iou(bboxes_a, bboxes_b, xyxy=True, GIoU=False, DIoU=False, CIoU=False
     from: https://github.com/chainer/chainercv
     https://github.com/ultralytics/yolov3/blob/eca5b9c1d36e4f73bf2f94e141d864f1c2739e23/utils/utils.py#L262-L282
     """
+    # 输入2个box
     if bboxes_a.shape[1] != 4 or bboxes_b.shape[1] != 4:
         raise IndexError
 
-    if xyxy:
+    if xyxy:  # 如果xyxy表示, 左上右下坐标 来表示一个box
         # intersection top left
         tl = torch.max(bboxes_a[:, None, :2], bboxes_b[:, :2])
         # intersection bottom right
         br = torch.min(bboxes_a[:, None, 2:], bboxes_b[:, 2:])
-        # convex (smallest enclosing box) top left and bottom right
+        # convex (smallest enclosing box) top left and bottom right   下面2行是算union的
         con_tl = torch.min(bboxes_a[:, None, :2], bboxes_b[:, :2])
         con_br = torch.max(bboxes_a[:, None, 2:], bboxes_b[:, 2:])
         # centerpoint distance squared
@@ -82,7 +84,7 @@ def bboxes_iou(bboxes_a, bboxes_b, xyxy=True, GIoU=False, DIoU=False, CIoU=False
 
         area_a = torch.prod(bboxes_a[:, 2:] - bboxes_a[:, :2], 1)
         area_b = torch.prod(bboxes_b[:, 2:] - bboxes_b[:, :2], 1)
-    else:
+    else:  # 另外一种格式 x,y,h,w 格式.
         # intersection top left
         tl = torch.max((bboxes_a[:, None, :2] - bboxes_a[:, None, 2:] / 2),
                        (bboxes_b[:, :2] - bboxes_b[:, 2:] / 2))
@@ -110,17 +112,17 @@ def bboxes_iou(bboxes_a, bboxes_b, xyxy=True, GIoU=False, DIoU=False, CIoU=False
     area_u = area_a[:, None] + area_b - area_i
     iou = area_i / area_u
 
-    if GIoU or DIoU or CIoU:
+    if GIoU or DIoU or CIoU:  #ciou最好.
         if GIoU:  # Generalized IoU https://arxiv.org/pdf/1902.09630.pdf
-            area_c = torch.prod(con_br - con_tl, 2)  # convex area
+            area_c = torch.prod(con_br - con_tl, 2)  # convex area  # 在第二维度上进行花间. 也就是求面积.
             return iou - (area_c - area_u) / area_c  # GIoU
         if DIoU or CIoU:  # Distance or Complete IoU https://arxiv.org/abs/1911.08287v1
-            # convex diagonal squared
+            # convex diagonal squared  # 引入中线点的距离这个变量.
             c2 = torch.pow(con_br - con_tl, 2).sum(dim=2) + 1e-16
             if DIoU:
                 return iou - rho2 / c2  # DIoU
             elif CIoU:  # https://github.com/Zzh-tju/DIoU-SSD-pytorch/blob/master/utils/box/box_utils.py#L47
-                v = (4 / math.pi ** 2) * torch.pow(torch.atan(w1 / h1).unsqueeze(1) - torch.atan(w2 / h2), 2)
+                v = (4 / math.pi ** 2) * torch.pow(torch.atan(w1 / h1).unsqueeze(1) - torch.atan(w2 / h2), 2) # 再引入长宽比差别这个变量.
                 with torch.no_grad():
                     alpha = v / (1 - iou + v)
                 return iou - (rho2 / c2 + v * alpha)  # CIoU
