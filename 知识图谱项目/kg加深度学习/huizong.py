d0364f2474adce72eea28fb936588689bd5e3db0
zhangbo2008 <15122306087@163.com>
2020-08-18 17:54:21 +0800

    no_comment

--- 知识图谱项目/kg加深度学习/huizong.py
+++ 知识图谱项目/kg加深度学习/huizong.py
@@ -98,13 +98,24 @@ kg里面:
 
 
 
-#---------------下面开始调用.
+#---------------下面开始调用!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 import torch
 
-kglist=['天津的大学','天津的人口','天津的面积']
+kglist=['大学','人口','面积']
+text='杀了姚明的人是谁'
+
+
+
 ##
 #--------从测试看出来,ner本身对问题有干扰,所以在kglist里面要去除.
-tiaozhuan=searchKG(kglist=['地点','地址','大小','颜色','老婆','丈夫'],text='我家住在和平区哪个地方')
+
+
+
+
+
+
+
+# tiaozhuan=searchKG(kglist=['地点','地址','大小','颜色','老婆','丈夫'],text='我家住在和平区哪个地方')
 
 
 
@@ -119,32 +130,71 @@ tiaozhuan=searchKG(kglist=['地点','地址','大小','颜色','老婆','丈夫'
 
 
 
-#
-#
-#
-# seg, hidden = ltp.seg([text])
+
+
+# 加入句子成分跳转.
+seg, hidden = ltp.seg([text])
 # sdp = ltp.sdp(hidden, graph=False)
-#
-# print(seg,"seg")
-# pos = ltp.pos(hidden)
-# ner = ltp.ner(hidden)
-# print("ner",ner)
-# srl = ltp.srl(hidden)
-# dep = ltp.dep(hidden)
-# sdp = ltp.sdp(hidden)
-#
-#
-#
-# seg=seg[0]
-# for i in sdp[0]:
-#
-#     print(i, seg[i[0]-1], seg[i[1]-1]) # 注意下标会多一个, 箭1后为真正下标.
-#
-#
-#
-#
+
+print(seg,"seg")
+pos = ltp.pos(hidden)
+ner = ltp.ner(hidden)
+print("ner",ner)
+srl = ltp.srl(hidden)
+dep = ltp.dep(hidden)
+sdp = ltp.sdp(hidden)
+
+print(ner,"ner结果")
+seg=seg[0]
+dep=dep[0]
+print(dep)
+for i in dep:
+
+    print(i, seg[i[0]-1], seg[i[1]-1]) # 注意下标会多一个, 箭1后为真正下标.
+
+'''
+下面我们根据跳跃图简历bfs算法
+'''
+
+#dep 就是我们需要的图
+# 从Ner出发,进行遍历图.找到他到root的一个路线.
+tiaozhuanlist={'ATT','SBV'} # 正序表
+tiaozhuanlist2={'VOB'} # 范旭表
+ner=ner[0]
+for ner_sample in ner:
+    # 然后对ner_sample进行跳跃搜索.
+
+    ner_sample_index=ner_sample[1]+1 # 变成dep图的索引类型.
+    luxian = [ner_sample_index]
+    def search_new_node():
+        ner_sample_index=luxian[-1]
+        for i in dep:
+            if i[0]==ner_sample_index and i[2] in tiaozhuanlist and i[1] not in luxian: # 进行的字跳转.并且防止循环.
+                luxian.append(i[1])
+                ner_sample_index=i[1]
+                return 1
+            if i[0]==ner_sample_index and i[2] in tiaozhuanlist2 and i[1] not in luxian: # 进行的字跳转.并且防止循环.
+                luxian.append(i[1])
+                ner_sample_index=i[1]
+                return 1
+            if i[1]==ner_sample_index and i[2] in tiaozhuanlist2 and i[0] not in luxian: # 进行的字跳转.并且防止循环. vob可能会反.也要考虑
+                luxian.append(i[0])
+                ner_sample_index=i[0]
+                return 1
+        return 0 # 说明找不到新跳跃了.
+    while search_new_node():
+        print("running")
+    print(luxian,"bfs方法找到的路线!!!!!!!!!!!!!!!")
 
 
+#--------------上面拿到路线了luxian, 在kg里面进行跳转即可.# 可以做词向量距离,来进行模糊跳转.
+
+
+# 如果luxian里面长度是1,说明没有找到跳转.只有ner.那么我们就用luxian里面这个.进入词向量.搜索算法即可.
+if len(luxian)==1:
+    #kglist = luxian[0] 这个东西的所有的边.
+    tiaozhuan = searchKG(kglist=['地点', '地址', '大小', '颜色', '老婆', '丈夫'], text=text)
+    # 利用距离小于一个阈值,我们就使用这个tiaozhuan,目前只支持bert算法的一次跳转,多次跳转没想到.
 
 
 
